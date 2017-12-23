3b0cbaff53c547387e9742df5105ac08710aae81
Matheus Ricelly <fsenaweb@gmail.com>
2017-12-23 20:19:47 -0300

    Commit da versão 0.4.1, com algumas correções

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ require(`quasar/dist/quasar.${__THEME}.css`)
 
 import Vue from 'vue'
 import Quasar from 'quasar'
-import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(Quasar) // Install Quasar Framework
@@ -29,7 +28,6 @@ Quasar.start(() => {
   /* eslint-disable no-new */
   new Vue({
     el: '#q-app',
-    router,
     render: h => h(require('./App').default)
   })
 })
